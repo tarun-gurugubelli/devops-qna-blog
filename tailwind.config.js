c1ea2c1d70ac3d814807c21675a803695aa30890
Tarun Gurugubelli <tarungurugubelli@outlook.com>
2025-02-19 17:03:01 +0530

    adding pacakge and tailwind config files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1E40AF', // A professional blue color
+        secondary: '#4F46E5', // A professional indigo color
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Use Inter font for a modern look
+      },
+    },
+  },
+  plugins: [],
+}
